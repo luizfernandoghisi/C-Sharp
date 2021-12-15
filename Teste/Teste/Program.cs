766df2b521eb9c38543c164dbf2774f2c9986c37
Luiz Fernando da Silveira Ghisi <fernandosilveiraghisi@gmail.com>
2021-12-15 11:46:31 -0300

    Criação do projeto Hello Word em Console
    Alterar a frase Hello Word para o nome próprio.

---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Luiz Fernando da Silveira Ghisi");
+        }
+    }
+}
